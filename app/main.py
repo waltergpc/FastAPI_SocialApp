2436c49ab8ef9c3cf06171006320b3a536844ee3
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-01-14 14:43:31 -0600

    set cors middleware

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ from .database import engine
 from . import models
 from .Routes import userRoutes, postRoutes, authRoutes, voteRoutes
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(authRoutes.router)
 app.include_router(postRoutes.router)
